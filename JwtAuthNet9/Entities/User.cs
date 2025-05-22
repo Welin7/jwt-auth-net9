6e9e692c4df86d18256140a3b1f16e00ee100a64
Welington Dias <33883532+Welin7@users.noreply.github.com>
2025-05-22 14:00:30 -0300

    feat: created entity user.

---
+++ JwtAuthNet9/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿namespace JwtAuthNet9.Entities
+{
+    public class User
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
