314dee6f1a42f3269b3b679d07046e44ec7e0c05
Welington Dias <33883532+Welin7@users.noreply.github.com>
2025-05-22 14:01:49 -0300

    feat: created controller auth.

---
+++ JwtAuthNet9/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using JwtAuthNet9.Dtos;
+using JwtAuthNet9.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthNet9.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+        
+        [HttpPost("register")]
+        public ActionResult<User> Register([FromBody] UserDto userDto)
+        {
+            var hashedPassword = new PasswordHasher<User>()
+                .HashPassword(user, userDto.Password);
+            
+            user.UserName = userDto.UserName;
+            user.PasswordHash = hashedPassword;
+            
+            return Ok(user);
+        }
+    }
+}
