ea475b12e2f9379e8d9a68e5a554807ef0794083
Welington Dias <33883532+Welin7@users.noreply.github.com>
2025-05-22 21:01:02 -0300

    feat: add applicationdbcontext.

---
+++ JwtAuthNet9/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using JwtAuthNet9.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuthNet9.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
