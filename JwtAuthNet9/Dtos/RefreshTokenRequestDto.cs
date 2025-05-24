de85ca3c9a67a419353f5ad9565c8fa97a67a300
Welington Dias <33883532+Welin7@users.noreply.github.com>
2025-05-24 00:55:06 -0300

    feat: created refresh token dto.

---
+++ JwtAuthNet9/Dtos/RefreshTokenRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace JwtAuthNet9.Dtos
+{
+    public class RefreshTokenRequestDto
+    {
+        public Guid UserId { get; set; }
+        public required string RefreshToken { get; set; }
+    }
+}
