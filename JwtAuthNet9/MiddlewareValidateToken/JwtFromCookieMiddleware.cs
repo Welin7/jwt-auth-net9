fef1f9433a917ca8d90951b0147978fda56f2123
Welington Dias <33883532+Welin7@users.noreply.github.com>
2025-05-30 16:46:42 -0300

    feat: created class middleware cookie.

---
+++ JwtAuthNet9/MiddlewareValidateToken/JwtFromCookieMiddleware.cs
@@ -0,0 +1,15 @@
+﻿namespace JwtAuthNet9.MiddlewareValidateToken
+{
+    public class JwtFromCookieMiddleware(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.Request.Cookies.TryGetValue("access_token", out var token))
+            {
+                context.Request.Headers.Authorization = $"Bearer {token}";
+            }
+
+            await next(context);
+        }
+    }
+}
